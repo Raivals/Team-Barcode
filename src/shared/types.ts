041af0df6e327809d2e24f7f703a1d32c0d336c1
Raivals <103427644+Raivals@users.noreply.github.com>
2023-03-05 16:18:45 +0100

    Navbar update MediaQueries

---
+++ src/shared/types.ts
@@ -0,0 +1,8 @@
+/*Exeption TS qui apparaît dans le code existant */
+
+export enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
