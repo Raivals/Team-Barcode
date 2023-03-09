539ec1f7dcac644f8ffc0630e50c0f9c5719cd2e
Raivals <103427644+Raivals@users.noreply.github.com>
2023-03-09 12:38:43 +0100

    Benefit page

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
