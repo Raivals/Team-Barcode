01bf8eabb5c9ce45f561ae499db657a3fffcd8cb
Raivals <103427644+Raivals@users.noreply.github.com>
2023-03-10 15:01:28 +0100

    Modif css Footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-priamry-100 py-16">
+    <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img src={Logo} alt="logo" />
