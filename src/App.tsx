60e9fabe06efb961ccebc2c9f37297fa9ad77ada
Raivals <103427644+Raivals@users.noreply.github.com>
2023-03-10 14:59:28 +0100

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
