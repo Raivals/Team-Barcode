0b89e88a31ce843f9b80584da8e353ce82260886
Raivals <103427644+Raivals@users.noreply.github.com>
2023-03-15 22:11:41 +0100

    First commit

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -5,38 +5,88 @@ import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
+import image7 from "@/assets/image7.png";
+import image8 from "@/assets/image8.png";
+import image9 from "@/assets/image9.png";
+import image10 from "@/assets/image10.png";
+import image11 from "@/assets/image11.png";
+import image12 from "@/assets/image12.png";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import Class from "./Class";
 
 const classes: Array<ClassType> = [
   {
-    name: "Weight Training Classes",
-    description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+    name: "Barcode",
+    description:
+      "Chef de la team Barcode. Les decks Backrows n'ont pas de secret pour lui.",
     image: image1,
   },
   {
-    name: "Yoga Classes",
+    name: "Boody",
+    description:
+      "Bras droit mais aussi possesseur de nombreux résultats en WCQ et YCS, le jeu n'a pas de secret pour lui.",
     image: image2,
   },
   {
-    name: "Ab Core Classes",
-    description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+    name: "Teto",
+    description:
+      "Cerveau de l'équipe, son art du deckbuilding est inspirant pour l'équipe. Aigris originel.",
+    image: image7,
+  },
+  {
+    name: "Thoumouche",
+    description:
+      "Prof de maths le jour, judge et coach la nuit, la polyvalence du joueur pendule n'a aucun égale.",
+    image: image8,
+  },
+  {
+    name: "Starz",
+    description:
+      "L'homme qui vendait les decks plus vite que son ombre. Swordsoul & life enjoyer, sa bonne humeur n'a aucun égal.",
+    image: image12,
+  },
+  {
+    name: "Zav",
+    description:
+      "Monteur officiel de la chaine de Barcode, son talent pour le montage vidéo reflète ses performances sur Yu-gi-Oh (sauf lorsqu'il est sujet de Budragon).",
     image: image3,
   },
   {
-    name: "Adventure Classes",
-    description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+    name: "Gaulard",
+    description:
+      "Sous ce beau visage ce cache le plus grand clowns de l'équipe. Lorsqu'il est concentré, c'est un adversaire redoutable.",
     image: image4,
   },
   {
-    name: "Fitness Classes",
+    name: "Raivals",
+    description:
+      "Développeur du site, coach mentale et spécialiste de la préparation des citrons.",
     image: image5,
   },
   {
-    name: "Training Classes",
-    description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.",
+    name: "Mist",
+    description:
+      "Amateur de Sky Striker, de grenouilles mais surtout de lait. Ne le sous-estimez pas il pourrait vous surprendre.",
     image: image6,
+  },
+  {
+    name: "Twarriorgenesis",
+    description:
+      "Fervant modérateur du discord et de twitch, amateur de Brave Token et de FTK. A tendance a insulter la daronne des gens mais toujours dans la bienveillance.",
+    image: image9,
+  },
+  {
+    name: "Katom",
+    description:
+      "Homme (ou plutôt jeune adolescent) qui a pour hobby principal de détruire ses adversaires à coups d'oiseaux. A pour habitude de jouer roleplay, comme en attestent les nombreux piafs qui peuplent son habitation.",
+    image: image10,
+  },
+  {
+    name: "Wingman",
+    description:
+      "Garde du corps de l'équipe. Les plays Baxia n'ont pas de secret pour lui. Gare à son Berserker du tenyi !!",
+    image: image11,
   },
 ];
 
@@ -46,7 +96,7 @@ type Props = {
 
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+    <section id="notreéquipe" className="w-full bg-primary-100 py-40">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
@@ -62,10 +112,7 @@ const OurClasses = ({ setSelectedPage }: Props) => {
           }}
         >
           <div className="md:w-3/5">
-            <HText>OUR CLASSES</HText>
-            <p className="py-5">
-              xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-            </p>
+            <HText>LA TEAM</HText>
           </div>
         </motion.div>
         <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
